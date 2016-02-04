0af78bf8ce3ebe7a6d33154aa5502278d609b804
Denis Lavrov <bahus.vel@gmail.com>
2016-02-04 14:54:27 +1300

    initial commit

---
+++ WhatNext/EventCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventCell.swift
+//  WhatNext
+//
+//  Created by denis lavrov on 4/02/16.
+//  Copyright © 2016 Denis Lavrov. All rights reserved.
+//
+
+import UIKit
+
+class EventCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
